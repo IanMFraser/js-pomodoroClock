7b69038d12d947ea756f070e634081ec1dbd15df
IanMFraser <hunter_gracchus@hotmail.com>
2019-11-18 13:21:04 -0500

    second commit

--- script.js
+++ script.js
@@ -19,5 +19,46 @@ let isClockRunning = false;
 let workSessionDuration = 1500; //25 minutes
 let currentTimeLeftInSession = 1500; //25 minutes
 let breakSessionDuration = 300; //5 minutes
+let type = 'work';
 
+const toggleClock = reset => {
+    if(reset){
+        //stop timer
+        stopClock();
+    } else {
+        if(isClockRunning === true){
+            //pause timer
+            clearInterval(clockTimer);
+            isClockRunning = false;
+        } else {
+            //start timer
+            isClockRunning = true;
+            clockTimer = setInterval(() => {
+                currentTimeLeftInSession--;
+                displayCurrentTimeLeftInSession();
+            }, 1000)
+        }
+    }
+}
 
+const displayCurrentTimeLeftInSession = () => {
+    const secondsLeft = currentTimeLeftInSession;
+    let result = '';
+    const seconds = secondsLeft % 60;
+    const minutes = parseInt(secondsLeft / 60) % 60;
+    let hours = parseInt(secondsLeft / 3600);
+
+    function addLeadingZeroes(time) {
+        return time < 10 ? `0${time}` : time
+    }
+    if (hours > 0) result += `${hours}:`
+    result += `${addLeadingZeroes(minutes)}:${addLeadingZeroes(seconds)}`
+    pomodoroTimer.innerText = result.toString();
+}
+
+const stopClock = () => {
+    clearInterval(clockTimer);
+    isClockRunning = false;
+    currentTimeLeftInSession = workSessionDuration;
+    displayCurrentTimeLeftInSession();
+}
